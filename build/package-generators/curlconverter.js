02e146b7ccda2ddb26b3755b49a28c74de51dfc5
boylin0 <as20757738@gmail.com>
2024-07-13 15:51:51 +0800

    fix: Resolve build issues for curlconverter on Windows using Yarn

--- build/package-generators/curlconverter.js
+++ build/package-generators/curlconverter.js
@@ -4,6 +4,7 @@ const child_process = require('child_process')
 const fs = require('fs')
 const path = require('path')
 
+const isWindows = process.platform === 'win32'
 const buildDir = execEnv.buildDir
 const packageJsonPath = path.join(buildDir, 'package.json')
 const projectDir = process.env.PROJECT_CWD
@@ -24,13 +25,20 @@ const version = '4.9.0'
 
 // Get URLs
 const tarballUrl = child_process
-  .execFileSync('npm', ['view', `curlconverter@${version}`, 'dist.tarball'])
+  .execFileSync(isWindows ? 'npm.cmd' : 'npm', [
+    'view',
+    `curlconverter@${version}`,
+    'dist.tarball',
+  ])
   .toString()
   .trim()
 const packageLockUrl = `https://raw.githubusercontent.com/curlconverter/curlconverter/v${version}/package-lock.json`
 
 // Download files
 const tarballPath = path.join(execEnv.tempDir, 'tarball.tgz')
+const tarballPathForCygwin = tarballPath
+  .replace(/\\/g, '/')
+  .replace(/^(\w):/, '/$1')
 const packageLockPath = path.join(buildDir, 'package-lock.json')
 child_process.execFileSync('curl', ['-s', tarballUrl, '-o', tarballPath])
 child_process.execFileSync('curl', [
@@ -42,17 +50,32 @@ child_process.execFileSync('curl', [
 
 // Extract tarball
 child_process.execFileSync(
-  'tar',
-  ['-xf', tarballPath, '--strip-components=1'],
+  isWindows ? 'tar.exe' : 'tar',
+  [
+    '-xf',
+    isWindows ? tarballPathForCygwin : tarballPath,
+    '--strip-components=1',
+  ],
   { cwd: buildDir },
 )
 
 // Remove all generators except json
-child_process.execFileSync(
-  'find',
-  ['-not', '-name', 'json.ts', '-path', './src/generators/*', '-delete'],
-  { cwd: buildDir },
-)
+if (isWindows) {
+  const files = fs.readdirSync(path.join(buildDir, 'src/generators'))
+  files.forEach(filename => {
+    const filepath = path.join(buildDir, 'src/generators', filename)
+    if (filename === 'json.ts') {
+      return
+    }
+    fs.rmSync(filepath, { recursive: true, force: true })
+  })
+} else {
+  child_process.execFileSync(
+    'find',
+    ['-not', '-name', 'json.ts', '-path', './src/generators/*', '-delete'],
+    { cwd: buildDir },
+  )
+}
 fs.writeFileSync(
   path.join(buildDir, 'src/index.ts'),
   'export { toJsonString } from "./generators/json.js";',
@@ -86,10 +109,12 @@ child_process.execFileSync('rm', [
 ])
 
 // Prepare package
-child_process.execFileSync('npm', ['install'], {
+child_process.execFileSync(isWindows ? 'npm.cmd' : 'npm', ['install'], {
   cwd: buildDir,
 })
-child_process.execFileSync('npm', ['run', 'compile'], { cwd: buildDir })
+child_process.execFileSync(isWindows ? 'npm.cmd' : 'npm', ['run', 'compile'], {
+  cwd: buildDir,
+})
 
 // Cleanup
 child_process.execFileSync('rm', ['-rf', path.join(buildDir, 'node_modules')])
